e7c4a20d736ff2e1471042bb81b904c96f982ca2
julioxbs <julioxbs@gmail.com>
2022-05-30 10:32:56 +0100

    inplemented progress bar and  current music

--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
 const controls = document.querySelector('#controls');
+const btnPlay = document.querySelector("#play-control");
 
 let index = 0;
 let currentMusic;
@@ -29,12 +30,66 @@ const creationOfAllMusics = (e) => {
             music.image = item.childNodes[1].childNodes[1].currentSrc;
             music.name = item.childNodes[3].childNodes[0].data;
             music.artist = item.childNodes[5].childNodes[0].data;
-            music.audio = item.childNodes[7].childNodes[1].currentSrc;
+            music.audio = item.childNodes[7].childNodes[1];
 
             audios.push(music);
             music = {};
         }
     });
 
-    console.log(audios);
+    const updateDataMusic = () => {
+        currentMusic = audios[index];
+        document.querySelector("#currentImg").src = currentMusic.image;
+        document.querySelector("#currentName").innerText = currentMusic.name;
+        document.querySelector("#currentArtist").innerText = currentMusic.artist;
+        document.querySelector("#volume").value = currentMusic.audio.volume * 100;
+
+        const progressBar = document.querySelector("#progressBar");
+        const textCurrentDuration = document.querySelector("#current-duration");
+        const textTotalDuration = document.querySelector("#total-duration");
+
+        progressBar.max = currentMusic.audio.duration;
+
+        textTotalDuration.innerText = secondsToMinutes(currentMusic.audio.duration);
+
+        textCurrentDuration.innerText = secondsToMinutes(currentMusic.audio.currentTime);
+
+        progressBar.valueAsNumber = currentMusic.audio.currentTime;
+    }
+
+
+    if (e.target.id === "play-control") {
+        if (index === 0) {
+            updateDataMusic();
+        }
+
+        if (!isPlaying) {
+            btnPlay.classList.replace("bi-play-fill", "bi-pause-fill");
+            currentMusic.audio.play();
+            isPlaying = true;
+            
+            setInterval(() => {
+                updatingProgressBar(currentMusic.audio.currentTime);
+            }, 1000);
+        } else {
+            btnPlay.classList.replace("bi-pause-fill", "bi-play-fill");
+            currentMusic.audio.pause();
+            isPlaying = false;
+        }
+    }
+}
+
+const updatingProgressBar = (time) => {
+    const textCurrentDuration = document.querySelector("#current-duration");
+    const progressBar = document.querySelector("#progressBar");
+
+    textCurrentDuration.innerText = secondsToMinutes(time);
+    progressBar.valueAsNumber = time;
+}
+
+const secondsToMinutes = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+
+    return `${("0" + minutes).slice(-2)}:${("0" + seconds).slice(-2)}`;
 }
